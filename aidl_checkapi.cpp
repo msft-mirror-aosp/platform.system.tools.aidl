d116f73665ebf7eb286941bbe6c1ad527eb5df13
Steven Moreland <smoreland@google.com>
2023-06-01 17:24:39 +0000

    checkapi: ignore unstable parcelables
    We can't do anything here, and Android Studio uses this.
    
    Bug: alanv@ mentioned over chat
    Test: N/A
    Change-Id: I7dad7df2272442c2847ff9c4eed8c14a769cfbf4

--- aidl_checkapi.cpp
+++ aidl_checkapi.cpp
@@ -507,6 +507,13 @@ bool check_api(const Options& options, const IoDelegate& io_delegate) {
       }
       compatible &= are_compatible_parcelables(*(old_type->AsStructuredParcelable()), *old_tns,
                                                *(new_type->AsStructuredParcelable()), *new_tns);
+    } else if (old_type->AsUnstructuredParcelable() != nullptr) {
+      // We could compare annotations or cpp_header/ndk_header here, but all these changes
+      // can be safe, and it's really up to the person making these changes to make sure
+      // they are safe. This is originally added for Android Studio. In the platform build
+      // system, this can never be reached because we build with '-b'
+
+      // ignore, do nothing
     } else if (old_type->AsUnionDeclaration() != nullptr) {
       if (new_type->AsUnionDeclaration() == nullptr) {
         AIDL_ERROR(new_type) << "Type mismatch: " << old_type->GetCanonicalName()
