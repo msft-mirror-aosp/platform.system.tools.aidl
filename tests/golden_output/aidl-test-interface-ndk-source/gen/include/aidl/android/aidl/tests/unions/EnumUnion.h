3eaf32602e8d4ea823e00a9f4c926c6ca073f1b8
Devin Moore <devinmoore@google.com>
2025-01-25 00:05:08 +0000

    Update golden output files
    Changes from https://r.android.com/3453515
    
    They aren't caught as necessary changes by the test hook because of
    b/351056624
    
    Test: m
    Bug: 351056624
    Bug: 367369897
    Change-Id: I07dc91793af8fd370e98f4fed0a2a4a82024f313

--- tests/golden_output/aidl-test-interface-ndk-source/gen/include/aidl/android/aidl/tests/unions/EnumUnion.h
+++ tests/golden_output/aidl-test-interface-ndk-source/gen/include/aidl/android/aidl/tests/unions/EnumUnion.h
@@ -60,7 +60,10 @@ public:
 
   EnumUnion() : _value(std::in_place_index<static_cast<size_t>(intEnum)>, ::aidl::android::aidl::tests::IntEnum(::aidl::android::aidl::tests::IntEnum::FOO)) { }
 
-  template <typename _Tp, typename = std::enable_if_t<_not_self<_Tp>>>
+  template <typename _Tp, typename = std::enable_if_t<
+      _not_self<_Tp> &&
+      std::is_constructible_v<std::variant<::aidl::android::aidl::tests::IntEnum, ::aidl::android::aidl::tests::LongEnum, int32_t>, _Tp>
+    >>
   // NOLINTNEXTLINE(google-explicit-constructor)
   constexpr EnumUnion(_Tp&& _arg)
       : _value(std::forward<_Tp>(_arg)) {}
