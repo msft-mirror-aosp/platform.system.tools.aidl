7a606a6a2bed9ea3a995bc69ad9b0b007ba03870
Pawan Wagh <waghpawan@google.com>
2025-06-09 20:37:36 +0000

    Don't generate zero sized code_to_function
    zero sized symbols can cause build errors depending upon
    the toolchain. Pass nullptr when there are no function names
    generated in array for code to function mapping and do not
    generate empty array.
    
    Test: m CtsNdkBinderTestCases && atest -c CtsNdkBinderTestCases
    Bug: 409851080
    Change-Id: I462d9a7168d26c172d00b5507cd0bb1382e79b1d

--- tests/golden_output/aidl-test-fixedsizearray-ndk-source/gen/android/aidl/fixedsizearray/FixedSizeArrayExample.cpp
+++ tests/golden_output/aidl-test-fixedsizearray-ndk-source/gen/android/aidl/fixedsizearray/FixedSizeArrayExample.cpp
@@ -1305,8 +1305,7 @@ static binder_status_t _aidl_android_aidl_fixedsizearray_FixedSizeArrayExample_I
   return _aidl_ret_status;
 }
 
-static const char* _g_aidl_android_aidl_fixedsizearray_FixedSizeArrayExample_IEmptyInterface_clazz_code_to_function[] = { };
-static AIBinder_Class* _g_aidl_android_aidl_fixedsizearray_FixedSizeArrayExample_IEmptyInterface_clazz = ::ndk::ICInterface::defineClass(FixedSizeArrayExample::IEmptyInterface::descriptor, _aidl_android_aidl_fixedsizearray_FixedSizeArrayExample_IEmptyInterface_onTransact, _g_aidl_android_aidl_fixedsizearray_FixedSizeArrayExample_IEmptyInterface_clazz_code_to_function, 0);
+static AIBinder_Class* _g_aidl_android_aidl_fixedsizearray_FixedSizeArrayExample_IEmptyInterface_clazz = ::ndk::ICInterface::defineClass(FixedSizeArrayExample::IEmptyInterface::descriptor, _aidl_android_aidl_fixedsizearray_FixedSizeArrayExample_IEmptyInterface_onTransact, nullptr, 0);
 
 FixedSizeArrayExample::BpEmptyInterface::BpEmptyInterface(const ::ndk::SpAIBinder& binder) : BpCInterface(binder) {}
 FixedSizeArrayExample::BpEmptyInterface::~BpEmptyInterface() {}
--- tests/golden_output/frozen/aidl-test-interface-ndk-source/gen/android/aidl/tests/ArrayOfInterfaces.cpp
+++ tests/golden_output/frozen/aidl-test-interface-ndk-source/gen/android/aidl/tests/ArrayOfInterfaces.cpp
@@ -61,8 +61,7 @@ static binder_status_t _aidl_android_aidl_tests_ArrayOfInterfaces_IEmptyInterfac
   return _aidl_ret_status;
 }
 
-static const char* _g_aidl_android_aidl_tests_ArrayOfInterfaces_IEmptyInterface_clazz_code_to_function[] = { };
-static AIBinder_Class* _g_aidl_android_aidl_tests_ArrayOfInterfaces_IEmptyInterface_clazz = ::ndk::ICInterface::defineClass(ArrayOfInterfaces::IEmptyInterface::descriptor, _aidl_android_aidl_tests_ArrayOfInterfaces_IEmptyInterface_onTransact, _g_aidl_android_aidl_tests_ArrayOfInterfaces_IEmptyInterface_clazz_code_to_function, 0);
+static AIBinder_Class* _g_aidl_android_aidl_tests_ArrayOfInterfaces_IEmptyInterface_clazz = ::ndk::ICInterface::defineClass(ArrayOfInterfaces::IEmptyInterface::descriptor, _aidl_android_aidl_tests_ArrayOfInterfaces_IEmptyInterface_onTransact, nullptr, 0);
 
 ArrayOfInterfaces::BpEmptyInterface::BpEmptyInterface(const ::ndk::SpAIBinder& binder) : BpCInterface(binder) {}
 ArrayOfInterfaces::BpEmptyInterface::~BpEmptyInterface() {}
